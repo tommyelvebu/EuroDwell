3edd7825ae7b943565ab0115fb7f3f273d957f4b
Tommy Elvebu <Tommy.elvebu@hotmail.com>
2025-05-30 10:50:28 +0200

    Updated Email functionality
    Changed email functionality to be generated to a .txt file instead of a message in the terminal.

--- djangoproject/mysite/settings.py
+++ djangoproject/mysite/settings.py
@@ -132,4 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
